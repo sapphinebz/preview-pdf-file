cc5370c2d7ef6a3231dee337e154bb9594dca3e7
Thanadit Buthong <thanadit.buthong@accenture.com>
2021-07-03 15:57:07 +0700

    build ใหม่และ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,35 @@
 import { Component, HostListener } from "@angular/core";
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 
 @Component({
   selector: "app-root",
   template: `
     <input #fileUpload type="file" (change)="onSelectFile($event)" />
-    <button (click)="previewPdf()">preview</button>
+    <button *ngIf="pdfFile" (click)="previewPdf()">preview</button>
+    <div>
+      <object
+        *ngIf="url"
+        id="preview"
+        height="800px"
+        width="100%"
+        [data]="url"
+        type="application/pdf"
+      ></object>
+    </div>
   `,
   styles: [],
 })
 export class AppComponent {
   title = "pdfWebView";
   pdfFile!: File;
+  url!: SafeUrl;
+
+  constructor(private domSanitizer: DomSanitizer) {}
 
   onSelectFile(event: any) {
     this.pdfFile = event.target.files[0];
+    const url = URL.createObjectURL(this.pdfFile);
+    this.url = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
   previewPdf() {
